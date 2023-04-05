b5afda22244bb3429e63551f7ff8966909c423a5
tgeb <tom.gebhardt@uni-rostock.de>
2023-04-05 10:21:31 +0200

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Test About</h1>
+</template>
+
+<script lang="ts">
+export default {
+    
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="bg-violet-600 text-white">
+    <header class="flex w-full gap-x-12">
+        <p>FAIRificator</p>
+        <a class="ml-auto">Home</a>
+        <a>Indicators</a>
+        <a>About</a>
+        <a>GitHub</a>
+    </header>
+
+    <div class="flex">
+        <div class="w-16 md:w-32 lg:w-48">
+            <h1>FAIRificator</h1>
+            <p>The FAIRificator aims to automate the evaluation of computational biology models and archives. </p>
+        </div>
+        <div class="w-16 md:w-32 lg:w-48">
+            <ol class="list-decimal list-inside marker:ring-4 marker:text-green-950 ring-4"> 
+                <li class="marker:ring-4">Enter the URL of a combine archive</li>
+                <li>Click "Start"</li>
+                <li>Automatic tests start</li>
+                <li>Respond to a few questions</li>
+                <li>Get your assesment results</li>
+            </ol>
+        </div>
+    </div>
+</div>
+
+
+</template>
+
+<script lang="ts">
+export default ({
+    
+})
+</script>
